936869d90a952ea393f53ada33c8f7025c7bcc96
Bellamcr <izabella.mcr@gmail.com>
2024-03-08 15:09:59 -0700

    demo day

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let recipeList = document.getElementById('recipe-list');
 let noRecipes = document.getElementById('no-recipes');
-let form = document.getElementById('form');
+let form = document.querySelector('form');
 
 let recipes = [];
 
@@ -8,13 +8,12 @@ function handleSubmit(event) {
     // Prevent default form submission behavior
     event.preventDefault();
 
-    let nameInput = document.getElementById('recipe-name');
-    let typeInput = document.getElementById('recipe-select');
-    let ingrInput = document.getElementById('recipe-ingredients');
-    let directionsInput = document.getElementById('recipe-directions');
-    let yieldsInput = document.getElementById('recipe-yields');
+    let nameInput = document.getElementById('recipeName');
+    let typeInput = document.getElementById('recipeSelect');
+    let ingrInput = document.getElementById('recipeIngredients');
+    let directionsInput = document.getElementById('recipeDirections');
+    let yieldsInput = document.getElementById('recipeYields');
     
-
     let name = nameInput.value.trim();
     let type = typeInput.value;
     let ingredients = ingrInput.value.trim().split(',').map(i => i.trim());
@@ -27,18 +26,13 @@ function handleSubmit(event) {
         recipes.push(newRecipe);
     }
 
-    console.log(recipes);
-
     nameInput.value = '';
     typeInput.value = '';
     ingrInput.value = '';
     directionsInput.value = '';
     yieldsInput.value = '';    
-    
-
+        
     displayRecipes();
-    console.log("recipe added");  
-    
 }
 
 form.addEventListener('submit', handleSubmit);
@@ -46,23 +40,61 @@ form.addEventListener('submit', handleSubmit);
 function displayRecipes() {
     recipeList.innerHTML = '';
     recipes.forEach((recipe, index) => {
-    let recipeDiv = document.createElement('div');
+    const recipeDiv = document.createElement('div'); 
+    
+    recipeDiv.innerHTML = `   
+    <h3>${recipe.name}</h3>
+    <p><strong>Type:</strong></p>
+    <p>${recipe.type}</p>
+    <p><strong>Yield:</strong></p>
+    <p>${recipe.yields}</p>
+    <button class="delete-button" data-index="${index}">Delete</button>`;
 
-    recipeDiv.innerHTML = `
-        <h3>${recipe.name}</h3>
-        <p><strong>Type:</strong></p>
-        <p>${recipe.type}</p>
-        <p><strong>Yield:</strong></p>
-        <p>${recipe.yields}</p>
-        <button class="delete-button" data-index="${index}">Delete</button>`;
- 
     recipeDiv.classList.add('recipe');
-    console.log("obj");
     recipeList.appendChild(recipeDiv);
     noRecipes.style.display = recipes.length > 0 ? 'none' : 'flex';
-    
-    }); 
+
+    });
+
 }
+
+
+
+
+    
+  
+
+// function viewRecipe() {
+//     recipes.forEach((recipe, index) => {
+    
+//     let showrecipeDiv = document.createElement('div');
+        
+//     showrecipeDiv.innerHTML = `
+//         <h3>${recipe.name}</h3>
+//         <p><strong>Ingredients:</strong></p>
+//         <p>${recipe.ingredients}</p>
+//         <p><strong>Directions:</strong></p>
+//         <p>${recipe.directions}</p>
+//         <button class="close-button" data-index="${index}">Close</button>`;
+        
+    
+//     showrecipeDiv.classList.add('show-recipe');
+
+//     });
+// }
+
+
+{/* <button class="delete-button" data-index="${recipes.indexOf(recipe)}">Delete</button> */}
+// function handleDelete(event) {
+//     if (event.target.classList.contains('delete-button')) {
+//         const index = event.target.dataset.index;
+//     }
+//     recipes.splice(index, 1);
+
+//     displayRecipes();
+// }
+
+// recipeList.addEventListener('click', handleDelete);
 
 
 {/* <img src=${recipe.photoUploaded}> */}
@@ -86,3 +118,27 @@ function displayRecipes() {
 //         }
 
 //     });
+
+{/* <button class="delete-button" data-index="${index}">Delete</button>
+<button id="show-recipe">View Recipe!</button>
+<div id="recipe-card">
+    <button id="hide-recipe">X</button>
+    <pre id="ingredient-con">${recipe.ingredients}</pre>
+    <pre id="directions">${recipe.directions}</pre>
+</div> */}
+
+    // let hideRecipe = document.getElementById("hide-recipe");
+    // let showRecipe = document.getElementById("show-recipe");
+    // let recipeCard = document.getElementById("recipe-card");
+    
+    
+    // hideRecipe.addEventListener("click", () => {
+    //     recipeCard.style.display = "none";
+    // });
+    // showRecipe.addEventListener("click", () => {
+    //    recipeCard.style.display = "block";
+    // });
+        
+    // recipeDiv.classList.add('recipe');
+    // recipeList.appendChild(recipeDiv);
+    // noRecipes.style.display = recipes.length > 0 ? 'none' : 'flex';
